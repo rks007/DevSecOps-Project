219f644ecd312c8e6bc15a0699adebbf5d83fbcd
Rishabh Kumar Singh <rishabhkumarsingh94@gmail.com>
2025-08-09 23:43:10 +0530

    add change in frontend

--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -14,11 +14,7 @@ export const AuthProvider = ({ children }) => {
 
   const login = async (email, password) => {
     try {
-<<<<<<< HEAD
-      const res = await axios.post('/auth/login', { email, password });
-=======
       const res = await axios.post('/api/auth/login', { email, password });
->>>>>>> b618d01754b2c63ee9ef65f3a29f2c26a274c3c4
       localStorage.setItem('token', res.data.token);
       localStorage.setItem('user', JSON.stringify(res.data.user));
       setToken(res.data.token);
@@ -34,11 +30,7 @@ export const AuthProvider = ({ children }) => {
 
   const register = async (name, email, password) => {
     try {
-<<<<<<< HEAD
-      await axios.post('/auth/register', { name, email, password });
-=======
       await axios.post('/api/auth/register', { name, email, password });
->>>>>>> b618d01754b2c63ee9ef65f3a29f2c26a274c3c4
       return { success: true };
     } catch (err) {
       return {
